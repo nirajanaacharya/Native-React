851f6b7889c1fbefa51e5d79c1204609e467cd96
nirajanaacharya <nirajanlegendkilker15@gmail.com>
2026-07-04 20:05:27 +0545

    Added MFA for sign-in

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,24 @@
+import { useAuth } from "@clerk/expo";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function profile() {
+  const router = useRouter();
+  const { signOut } = useAuth();
+  const handleSignout = async () => {
+    try {
+      await signOut();
+      router.replace("/sign-in");
+    } catch (error) {
+      console.error("Error Signing out", error);
+    }
+  };
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Profile Page</Text>
+      <TouchableOpacity onPress={handleSignout}>Log Out</TouchableOpacity>
+    </SafeAreaView>
   );
 }
